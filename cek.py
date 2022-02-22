360ef2930e2a155c4e85f115dd9a420792806aa3
gonblok <99801071+gonblok@users.noreply.github.com>
2022-02-23 04:08:00 +0700

    Update cek.py

--- cek.py
+++ cek.py
@@ -1,24 +1,20 @@
-from datetime import date
-from pycoingecko import CoinGeckoAPI
+import os
+import telebot
 import requests as r
-import time
-def cek():
-	today = date.today()
-	d3 = today.strftime("%m/%d/%y")
-	print(type(d3))
-	print(d3)
-	cg = CoinGeckoAPI()
-	print("Trending Koin di CoinGecko\n")
-	koin = cg.get_search_trending()
-	koinl = koin.get("coins")
-	r.get("https://api.telegram.org/bot5153674664:AAGIf3UvyN4cA1cLsvakQNmQSY7DUsEMfss/sendMessage?chat_id=2071025459&text=DAFTAR KOIN TRENDING DI COINGECKO {}".format(d3))
-	for i in range(7):
-		c = i+1
-		hasil =str(c)+"."+koinl[i].get("item")['name']
-		print(hasil)
-		r.get("https://api.telegram.org/bot5153674664:AAGIf3UvyN4cA1cLsvakQNmQSY7DUsEMfss/sendMessage?chat_id=2071025459&text={}".format(hasil))
+import json
 
-while True:
-	cek()
-	time.sleep(900)
-#r.get("https://api.telegram.org/bot5153674664:AAGIf3UvyN4cA1cLsvakQNmQSY7DUsEMfss/sendMessage?chat_id=2071025459&text=---------------------------------------------------------------------------------------")
+def cek():
+  global hargaj
+  data = r.get("https://indodax.com/api/tickers")
+  data1 = json.loads(data.content.decode('utf-8'))
+  harga = 'btc_idr'.replace("_idr",""),data1['tickers']['btc_idr']['last']
+  hargaj = '  :  '.join(harga)
+
+API_KEY = '5153674664:AAGIf3UvyN4cA1cLsvakQNmQSY7DUsEMfss'
+bot = telebot.TeleBot(API_KEY)
+@bot.message_handler(commands=['info'])
+def info(message):
+  cek()
+  bot.reply_to(message,hargaj)
+
+bot.polling()
